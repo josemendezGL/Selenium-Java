7155a98b0c96f0115daeb939e8ce34e2541c39d1
Jose Mendez <jose.mendez@gorillalogic.com>
2024-06-18 17:49:15 -0600

    CustomWebDriverManager added

--- src/test/java/com/example/stepdefinitions/ui/LoginSteps.java
+++ src/test/java/com/example/stepdefinitions/ui/LoginSteps.java
@@ -1,18 +1,13 @@
 package com.example.stepdefinitions.ui;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import com.example.pageobjects.DashboardPage;
 import com.example.pageobjects.LandingPage;
 import com.example.pageobjects.LoginPage;
-import java.time.Duration;
-import io.cucumber.java.Before;
-import io.cucumber.java.After;
+import com.example.utils.CustomWebDriverManager;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 import static org.junit.Assert.*;
 
@@ -28,15 +23,7 @@ public class LoginSteps {
 
     @Given("user is on Login page")
     public void user_is_on_login_page() {
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.chromedriver().browserVersion("126.0.6478.62").setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--remote-allow-origins=*"); // Agrega esta línea
-        // options.addArguments("--headless"); // Si deseas ejecutar en modo sin cabeza
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // espera implícita de 10 segundos
+        driver = CustomWebDriverManager.getDriver();
         driver.get("https://trello.com/");
         landingPage = new LandingPage(driver);
     }
@@ -52,6 +39,6 @@ public class LoginSteps {
     public void user_is_navigated_to_the_home_page() {
         dashboardPage = new DashboardPage(driver);
         assertTrue(dashboardPage.isBoardsButtonVisible());
-        driver.quit();
+        CustomWebDriverManager.quitDriver();
     }
 }
